92ab85f7f4c1959cfff4b4d14acb879ff8333724
mohitbajeli <mohitbajeli7617@gmail.com>
2023-08-11 22:05:30 +0530

    c

--- ques/evenodd.cpp
+++ ques/evenodd.cpp
@@ -6,12 +6,25 @@ bool oddeven(int n)
 {
     if(n&1)
     {
-       return true;
+       return false;
     }
-    return false;
+    return true;
 }
 
 int main()
 {
-    
+    int n;
+    cout<<"enter the no"<<endl;
+    cin>>n;
+
+    if(oddeven(n))
+    {
+        cout<<"no is even"<<endl;
+    }
+    else
+    {
+        cout<<"no os odd"<<endl;
+    }
+
+    return 0;
 }
