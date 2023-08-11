8bd38f6a95e31c33ddfc5aee93b5f8414f978611
mohitbajeli <mohitbajeli7617@gmail.com>
2023-08-11 21:57:16 +0530

    c

--- ques/power2.cpp
+++ ques/power2.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 bool ispower(int n)
 {
+    int ans=1;
     for(int i=0;i<=30;i++)
     {
-        int ans=pow(2,i);
         if(ans==n)
         {
             return true;
         }
+        ans=ans*2;
     }
     return false;
 }
