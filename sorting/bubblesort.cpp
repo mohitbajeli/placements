053c112381ea10d07a41bda7b3224891d198c96f
mohitbajeli <mohitbajeli7617@gmail.com>
2023-09-03 17:12:27 +0530

    c

--- sorting/bubblesort.cpp
+++ sorting/bubblesort.cpp
@@ -11,7 +11,7 @@ void swap(int *a,int *b){
 void bubblesort(int arr[],int n){
 
     for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i;j++)
+        for(int j=0;j<n-i-1;j++)
         {
             if(arr[j]>arr[j+1]){
                 swap(&arr[j],&arr[j+1]);
