e10accb100bebed396fe65b0558d4703aaa3b079
mohitbajeli <mohitbajeli7617@gmail.com>
2023-09-27 11:40:09 +0530

    c

--- sorting/mergesort.cpp
+++ sorting/mergesort.cpp
@@ -4,17 +4,60 @@ using namespace std;
 
 void merge(int arr[],int lb,int mid,int ub){
     
-}
+    int k=lb;
+    int b[50];
+    int i=lb;
+    int j=mid+1;
+    while(i<=mid && j<=ub){
+        if(arr[i]<=arr[j]){
+            b[k]=arr[i];
+            i++;
+            k++;
+        }
+        else{
+            b[k]=arr[j];
+            k++;
+            j++;
+        }
+    }
+    
+        while(j<=ub){
+            b[k]=arr[j];
+            k++;
+            j++;
+        }
+    
+    
+        while(i<=mid){
+            b[k]=arr[i];
+            k++;
+            i++;
+        }
 
+
+    for(int i=lb;i<k;i++){
+        arr[i]=b[i];
+    }
+
+}
 void mergesort(int arr[],int lb,int ub){
     if(lb<ub){
-        mid = lb+(ub-lb)/2;
+       int mid = lb+(ub-lb)/2;
         mergesort(arr,lb,mid);
-        mergsort(arr,mid+1,ub);
+        mergesort(arr,mid+1,ub);
         merge(arr,lb,mid,ub);
     }
 }
 
 int main(){
+int arr[]={15,5,24,8,1,3,16,10,20};
 
+
+mergesort(arr,0,8);
+
+cout<<"sorted array is "<<endl;
+
+for(int i=0;i<9;i++){
+    cout<<arr[i]<<" ";
+}
 }
