21dd5e7fa037d97444ba533a0e2b67d1b61d0448
mohitbajeli <mohitbajeli7617@gmail.com>
2023-09-28 11:55:26 +0530

    c

--- recursion.cpp/permutationstring.cpp
+++ recursion.cpp/permutationstring.cpp
@@ -2,13 +2,38 @@
 
 using namespace std;
 
-vector<string>permutation(string &nums,int n){
-    vector<string>ans;
-    
+void solve(vector<int>nums, vector<vector<int>>&ans,int index){
+    if(index>=nums.size()){
+        ans.push_back(nums);
+        return;
+
+    }
+
+    for(int j=index;j<nums.size();j++){
+        swap(nums[j],nums[index]);
+        solve(nums,ans,index+1);
+        swap(nums[j],nums[index]);
+    }
+}
+
+vector<vector<int>>permute(vector<int> &nums){
+    vector<vector<int>> ans;
+    int index=0;
+    solve(nums,ans,index);
+
+    return ans;
 }
 
 int main(){
-    string nums = "abc";
+    vector<int>nums={1,2,3};
+    vector<vector<int>>ans=permute(nums);
+
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j];        
+        }
+    cout<<endl;
+    }
 
 
 }
