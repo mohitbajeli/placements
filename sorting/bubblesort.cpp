2377186e0c1629d625f48ce4750c3964fd856b7b
mohitbajeli <mohitbajeli7617@gmail.com>
2023-11-24 13:27:23 +0530

    c

--- sorting/bubblesort.cpp
+++ sorting/bubblesort.cpp
@@ -10,10 +10,15 @@ void swap(int *a,int *b){
 void bubblesort(int arr[],int n){
 
     for(int i=0;i<n-1;i++){
+        bool flag=false;
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(&arr[j],&arr[j+1]);
+                flag=true;
             }
+        }
+        if(flag){
+            break;
         }
     }
 }
