9bee9e2a344d4b33724f991dade243a5bbf700fa
mohitbajeli <mohitbajeli7617@gmail.com>
2023-11-28 19:12:21 +0530

    c

--- arrays/bookallocation.cpp
+++ arrays/bookallocation.cpp
@@ -1,38 +1,39 @@
 #include<bits/stdc++.h>
 
-bool ispossible(int arr[],int n,int m,int mid){
-int studentcount=1;
-int pagesum=0;
-for(int i=0;i<n;i++){
-    if(pagesum+arr[i]<=mid ){
-          pagesum+=arr[i];
-    }
-    else{
-        studentcount++;
+using namespace std;
 
-        if(studentcount>m || arr[i]>mid){
-            return false;
+bool ispossible(int arr[],int n,int m,int mid){
+    int studentcount=1;
+    int pagesum=0;
+    for(int i=0;i<n;i++){
+        if(pagesum+arr[i]<=mid){
+            pagesum+=arr[i];
         }
-        pagesum=arr[i];
-    }
+        else{
+            studentcount++;
+            if(studentcount>m || arr[i]>mid){
+            return false;
+            }
+            pagesum=arr[i];
+        }
     }
     return true;
-
 }
 
-int book(int arr[],int n,int m){
+int allocation(int arr[],int n,int m){
     int s=0;
     int sum=0;
     for(int i=0;i<n;i++){
         sum+=arr[i];
     }
-    int ans=0;
+    int ans=-1;
     int e=sum;
-    int mid=s+(e-s)/2;
+    int mid = s+(e-s)/2;
     while(s<=e){
         if(ispossible(arr,n,m,mid)){
             ans=mid;
             e=mid-1;
+
         }
         else{
             s=mid+1;
@@ -42,12 +43,8 @@ int book(int arr[],int n,int m){
     return ans;
 }
 
-
-using namespace std;
-
-int main()
-{
-    int arr[4]={5,5,5,5};
-    int m=2;
-   cout<<"minimum is "<<book(arr,4,m);
+int main(){
+    int arr[]={10,20,30,40};
+    cout<<"max no of pages allocated which is minimum is "<<allocation(arr,4,2);
+    return 0;
 }
