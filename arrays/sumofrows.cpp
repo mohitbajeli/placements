b71e0974b2fa3f4b8feda1da1355da8c8539e790
mohitbajeli <mohitbajeli7617@gmail.com>
2023-12-01 22:46:19 +0530

    c

--- arrays/sumofrows.cpp
+++ arrays/sumofrows.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+void sumcolumn(int arr[][3],int m,int n){
+    for(int i=0;i<m;i++){
+        int sum=0;
+        for(int j=0;j<n;j++){
+            sum+=arr[j][i];
+        }
+        cout<<"sum of "<<i+1<<" column is "<<sum<<endl;
+    }
+}
+
 void sumrow(int arr[][3],int m,int n){
     for(int i=0;i<m;i++){
         int sum=0;
@@ -12,9 +22,12 @@ void sumrow(int arr[][3],int m,int n){
     }
 }
 
+
 int main(){
     int arr[3][3]={{3,4,11},{2,12,1},{7,8,7}};
 
     sumrow(arr,3,3);
+    cout<<endl;
+    sumcolumn(arr,3,3);
     return 0;
 }
