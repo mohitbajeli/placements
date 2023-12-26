a8979750669bdb571913ee65754e369d25a6627e
mohitbajeli <mohitbajeli7617@gmail.com>
2023-12-26 13:13:43 +0530

    c

--- recursion.cpp/powerset.cpp
+++ recursion.cpp/powerset.cpp
@@ -3,18 +3,14 @@
 using namespace std;
 
 void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>output,int index){
-
+    
     if(index>=nums.size()){
-        
         ans.push_back(output);
-        return ;
+        return;
     }
-
     solve(nums,ans,output,index+1);
-
-    int element = nums[index];
+    int element=nums[index];
     output.push_back(element);
-
     solve(nums,ans,output,index+1);
 }
 
@@ -27,7 +23,7 @@ int main(){
 
     for(int i=0;i<ans.size();i++){
         for(int j=0;j<ans[i].size();j++){
-            cout<<ans[i][j];
+            cout<<ans[i][j]<<" ";
         }
         cout<<endl;
     }
